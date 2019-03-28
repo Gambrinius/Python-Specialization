b554d7d0339db37c784e8b0296acc83261933c28
Gambrinius <ilya.konon.95@gmail.com>
2019-03-28 17:51:58 +0300

    update diguts_sum.py realization

--- diving-into-python/week1/digits_sum.py
+++ diving-into-python/week1/digits_sum.py
@@ -3,8 +3,6 @@ import sys
 digit_string = sys.argv[1]
 
 if digit_string.isdigit():
-    result = 0
-    for char in digit_string:
-        result += int(char)
+    result = sum([int(char) for char in digit_string])
 
     print(result)
